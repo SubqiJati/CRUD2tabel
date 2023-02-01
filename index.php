b273051493938b9c45cc3b2a36b802af102cb35e
SubqiJati <109190944+SubqiJati@users.noreply.github.com>
2023-02-01 15:16:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMK N1 SAYUNG</title>
+</head>
+<body>
+<hr />
+    <header style="text-align: center;">
+        <img src="1111.jfif" width="200" height="200" style="border-radius: 50%;"/>
+        <h1>SMKN 1 SAYUNG DEMAK</h1>
+    </header>
+    <hr />
+    <article style="text-align: center;">
+        <h2>MENU SMKN 1 SAYUNG DEMAK</h2>
+    </article>
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>MENU</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+            <ul>
+            <li> <a href = "tambah.php">TAMBAH PEMBIMBING</a> </li>
+</ul>
+</tr>
+</td>
+            </tbody>
+        </table>
+    </div>
+    <hr>
+    
+</body>
+</html>
+</body>
+</html>
